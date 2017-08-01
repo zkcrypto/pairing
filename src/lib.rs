766c902d8c0650e7c72870aeb6168024added33c
Sean Bowe <ewillbefull@gmail.com>
2017-08-01 11:37:51 -0600

    Allow `unreadable_literal` lint.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 #![cfg_attr(feature = "clippy", allow(inline_always))]
 #![cfg_attr(feature = "clippy", allow(too_many_arguments))]
+#![cfg_attr(feature = "clippy", allow(unreadable_literal))]
 
 // The compiler provides `test` (on nightly) for benchmarking tools, but
 // it's hidden behind a feature flag. Enable it if we're testing.

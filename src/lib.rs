85b95750e2b0ed6cf7288f4ce16d6e0cb060fdaf
Sean Bowe <ewillbefull@gmail.com>
2017-10-04 14:09:40 -0600

    Fix comment about u128-support.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-// This library relies on the Rust nightly compiler's `i128_type` feature.
-// If that's not okay for you, disable the u128-support feature. (Pass
-// --no-default-features for example.)
+// If the "u128-support" feature is enabled, this library can use
+// more efficient arithmetic. Only available in the nightly compiler.
 #![cfg_attr(feature = "u128-support", feature(i128_type))]
 
 // `clippy` is a code linting tool for improving code quality by catching
